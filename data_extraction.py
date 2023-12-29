f45f13521abd0c62f2d8b66d12d541d0c3ade492
Oleksii Maniachenko <48956178+Maniachenko@users.noreply.github.com>
2023-12-29 13:37:46 +0100

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -4,14 +4,22 @@ import pandas as pd
 from tqdm import tqdm
 from tree_sitter import Language, Parser
 
+JAVA_EXTENSIONS = [".java"]
+ENCODINGS = ['utf-8', 'ISO-8859-1', 'windows-1252', 'utf-16']
+
 
 # Load Java language parser
 def load_java_parser():
+    """
+    Builds and loads the Java language parser using tree-sitter.
+    Returns: Parser object for Java language.
+    """
     Language.build_library('build/my-languages.so', ['tree-sitter-java'])
     java_language = Language('build/my-languages.so', 'java')
     parser = Parser()
     parser.set_language(java_language)
     return parser
+
 
 # Function to recursively traverse the syntax tree
 def extract_methods(node):
@@ -54,37 +62,49 @@ def extract_methods(node):
     return methods
 
 
-# Function to parse a Java file
-def parse_java_file(file_path, parser):
-    # Trying different encodings to read the file
-    encoding_to_try = ['utf-8', 'ISO-8859-1', 'windows-1252', 'utf-16']
-    for encoding in encoding_to_try:
+def read_file(file_path):
+    """
+    Tries to read the file with different encodings.
+    Args:
+        file_path (str): Path to the file.
+    Returns:
+        str: Contents of the file.
+    Raises:
+        RuntimeError: If all encodings fail.
+    """
+    for encoding in ENCODINGS:
         try:
             with open(file_path, 'r', encoding=encoding) as file:
-                source_code = file.read()
-            # Parsing the Java source code to a syntax tree
-            tree = parser.parse(bytes(source_code, "utf8"))
-            root_node = tree.root_node
-            # Extracting methods from the root node of the syntax tree
-            methods = extract_methods(root_node)
-
-            # Associating each extracted method with its source file path
-            for method in methods:
-                method['file_path'] = file_path
-
-            return methods
+                return file.read()
         except UnicodeDecodeError:
-            # Trying next encoding if current one fails
             continue
-    else:
-        # Raising an exception if all encoding attempts fail
-        raise Exception("Failed to decode file with tried encodings")
+    raise RuntimeError(f"Failed to decode file: {file_path}")
+
+
+def parse_java_file(file_path, parser):
+    """
+    Parses a Java file and extracts method information.
+    Args:
+        file_path (str): Path to the Java file.
+        parser (Parser): tree-sitter Parser for Java.
+    Returns:
+        list: List of dictionaries, each containing method information.
+    """
+    source_code = read_file(file_path)
+    tree = parser.parse(bytes(source_code, "utf8"))
+    root_node = tree.root_node
+    methods = extract_methods(root_node)
+    for method in methods:
+        method['file_path'] = file_path
+    return methods
+
 
 # Main function to process all Java files in a directory
 def process_java_files(directory):
     java_files = []
     for dir_path, dir_names, file_names in os.walk(directory):
-        java_files.extend([os.path.join(dir_path, file_name) for file_name in file_names if file_name.endswith(".java")])
+        java_files.extend(
+            [os.path.join(dir_path, file_name) for file_name in file_names if file_name.endswith(".java")])
 
     all_methods = []
     parser = load_java_parser()
@@ -93,12 +113,14 @@ def process_java_files(directory):
         methods_list = parse_java_file(file, parser)
         all_methods.extend(methods_list)
 
-    methods_df = pd.DataFrame(all_methods, columns=['identifier', 'formal_parameters', 'modifiers', 'block', 'type_identifier',
-                                                    "boolean_type", "floating_point_type", "generic_type", "scoped_type_identifier",
-                                                    "integral_type", "void_type", 'file_path'])
+    methods_df = pd.DataFrame(all_methods,
+                              columns=['identifier', 'formal_parameters', 'modifiers', 'block', 'type_identifier',
+                                       "boolean_type", "floating_point_type", "generic_type", "scoped_type_identifier",
+                                       "integral_type", "void_type", 'file_path'])
 
     methods_df.to_csv('java_methods.csv')
 
+
 # Execute the script
 if __name__ == "__main__":
-    process_java_files('/content/intellij-community')
+    process_java_files('intellij-community')
